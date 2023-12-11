2f87ac47248ac0ceba84a9b665c21e0d945b660f
Giang <nguyenvietgiang1110@gmail.com>
2023-12-11 20:22:25 +0700

    Add project files.

---
+++ DoAnTotNghiep/Models/EntityModels/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+
+namespace DoAnTotNghiep.Models.EntityModels
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Candidate> Candidates { get; set; }
+        public DbSet<Employer> Employers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
+     
---
+++ DoAnTotNghiep/Models/EntityModels/Employer.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DoAnTotNghiep.Models.EntityModels
+{
+    public class Employer
+    {
+        [Key]
+        public Guid EmployerID { get; set; }
+
+        [Required]
+        public string CompanyName { get; set; }
+        public string? UrlImage { get; set; }
+        public string? Industry { get; set; } 
+        public int? CompanySize { get; set; }
+        public string? Location { get; set; }
+
+        [ForeignKey("EmployerID")]
+        public Account Account { get; set; }
+    }
+}
