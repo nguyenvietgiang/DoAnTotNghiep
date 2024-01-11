75aafe4ad80d568a2abb26ffcf07370e419f48e2
Giang <nguyenvietgiang1110@gmail.com>
2024-01-11 22:59:50 +0700

    gợi ý khi tìm kiếm và hiện khi không tìm thấy kết quả

--- DoAnTotNghiep/Controllers/MvcController/SearchController.cs
+++ DoAnTotNghiep/Controllers/MvcController/SearchController.cs
@@ -1,16 +1,20 @@
-﻿using DoAnTotNghiep.Models.ResponseDTO;
+﻿using DoAnTotNghiep.Models.EntityModels;
+using DoAnTotNghiep.Models.ResponseDTO;
 using DoAnTotNghiep.Repository.JobRepo;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DoAnTotNghiep.Controllers.MvcController
 {
     public class SearchController : Controller
     {
         private readonly IJobPostingRepository _jobPostingRepository;
+        private readonly DataContext _dataContext;
 
-        public SearchController(IJobPostingRepository jobPostingRepository)
+        public SearchController(IJobPostingRepository jobPostingRepository, DataContext dataContext)
         {
             _jobPostingRepository = jobPostingRepository;
+            _dataContext = dataContext;
         }
 
         public IActionResult SearchResult()
@@ -19,7 +23,7 @@ namespace DoAnTotNghiep.Controllers.MvcController
         }
 
         [HttpPost]
-        public async Task<IActionResult> SearchJob(string searchTerm, string location)
+        public async Task<IActionResult> SearchJob(string? searchTerm, string? location)
         {
             var jobPostings = await _jobPostingRepository.SearchJobPostingsAsync(searchTerm, location);
             var result = jobPostings.Select(jp => new JobPostingResponseDto
@@ -46,6 +50,28 @@ namespace DoAnTotNghiep.Controllers.MvcController
             }
 
             return View(jobPosting);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTitlesBySearch(string searchString)
+        {
+            IQueryable<string> query = _dataContext.JobPostings
+                                        .Where(m => m.Status == true && m.Title.Contains(searchString))
+                                        .Select(m => m.Title);
+
+            var titles = await query.ToListAsync();
+            return Json(titles);
+        } 
+
+        [HttpGet]
+        public async Task<IActionResult> GetCompanyBySearch(string searchString)
+        {
+            IQueryable<string> query = _dataContext.Employers
+                                        .Where(m => m.CompanyName.Contains(searchString))
+                                        .Select(m => m.CompanyName);
+
+            var titles = await query.ToListAsync();
+            return Json(titles);
         }
     }
 }
