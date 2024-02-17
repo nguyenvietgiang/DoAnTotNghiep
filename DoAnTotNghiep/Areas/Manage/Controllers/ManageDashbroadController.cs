0e8f2e14ef2a33ad7c48f35c586d82d76c860811
Giang <nguyenvietgiang1110@gmail.com>
2024-02-17 22:32:12 +0700

    Thống kê chart doanh thu theo từng năm

--- DoAnTotNghiep/Areas/Manage/Controllers/ManageDashbroadController.cs
+++ DoAnTotNghiep/Areas/Manage/Controllers/ManageDashbroadController.cs
@@ -14,6 +14,8 @@ namespace DoAnTotNghiep.Areas.Manage.Controllers
         }
         public IActionResult Index()
         {
+            int currentYear = DateTime.Now.Year;
+            ViewBag.CurrentYear = currentYear;
             ViewBag.Employer = _dataContext.Employers.Count();
             ViewBag.Cadidates = _dataContext.Candidates.Count();
             ViewBag.Job = _dataContext.JobPostings.Count();
@@ -21,14 +23,16 @@ namespace DoAnTotNghiep.Areas.Manage.Controllers
             return View();
         }
 
-        public IActionResult GetRevenueData()
+        public IActionResult GetRevenueData(int selectedYear)
         {
             var revenueData = _dataContext.RevenueStatistics
+                .Where(r => r.Date.Year == selectedYear)
                 .OrderBy(r => r.Date)
                 .Select(r => new { Date = r.Date.ToString("yyyy-MM-dd"), Amount = r.Amount })
                 .ToList();
 
             return Json(revenueData);
         }
+
     }
 }
