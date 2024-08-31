6c924cece455c021c4c553a242e36c8134e209cc
Giang <nguyenvietgiang1110@gmail.com>
2024-08-31 18:32:30 +0700

    XZING gen qr code (đã gen được và chuyển thành base 64 nhưng không hiện ở mail)

--- DoAnTotNghiep/Services/EmailServices/EmailServices.cs
+++ DoAnTotNghiep/Services/EmailServices/EmailServices.cs
@@ -1,6 +1,10 @@
 ﻿using MimeKit;
 using System.Net.Mail;
 using System.Net;
+using ZXing;
+using ZXing.QrCode;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace DoAnTotNghiep.Services.EmailServices
 {
@@ -12,6 +16,33 @@ namespace DoAnTotNghiep.Services.EmailServices
             message.From.Add(MailboxAddress.Parse("nguyenvietgiang1110@gmail.com"));
             message.To.Add(MailboxAddress.Parse(email));
             message.Subject = "Job Finder";
+
+            // Generate QR Code using ZXing
+            var writer = new BarcodeWriterPixelData
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = new QrCodeEncodingOptions
+                {
+                    Height = 300,
+                    Width = 300,
+                    Margin = 1
+                }
+            };
+            var pixelData = writer.Write("http://localhost:5044/");
+            using var bitmap = new Bitmap(pixelData.Width, pixelData.Height, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+            using var ms = new MemoryStream();
+            var bitmapData = bitmap.LockBits(new System.Drawing.Rectangle(0, 0, pixelData.Width, pixelData.Height), ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+            try
+            {
+                System.Runtime.InteropServices.Marshal.Copy(pixelData.Pixels, 0, bitmapData.Scan0, pixelData.Pixels.Length);
+            }
+            finally
+            {
+                bitmap.UnlockBits(bitmapData);
+            }
+            bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            var qrCodeBase64 = Convert.ToBase64String(ms.ToArray());
+
             var bodyBuilder = new BodyBuilder();
             bodyBuilder.HtmlBody = $@"
                 <html>
@@ -46,6 +77,14 @@ namespace DoAnTotNghiep.Services.EmailServices
                             .footer {{
                                 text-align: center;
                             }}
+                     .qr-code {{
+                        text-align: center;
+                        margin-top: 20px;
+                    }}
+                    .qr-code img {{
+                        width: 100px;
+                        height: 100px;
+                    }}
                         </style>
                     </head>
                     <body>
@@ -62,6 +101,9 @@ namespace DoAnTotNghiep.Services.EmailServices
                             <div class=""footer"">
                                 <p>Best Regast,</p>
                                 <p>Nguyễn Việt Giang</p>
+                        <div class=""qr-code"">
+                            <img src=""data:image/png;base64,{qrCodeBase64}"" alt=""QR Code"">
+                        </div>
                             </div>
                         </div>
                     </body>
